fa3f15dffe550efd1a5c15c3565eee9b96b46bc3
Alejandro Garcia <alejandro.nocedo@gmail.com>
2020-08-02 16:25:25 +0200

    creacion de lienzos

--- d3.js
+++ d3.js
@@ -1,8 +1,33 @@
 //Declaracion de variables globales
+var maph = 500
+var mapw = 500
+var graphh = 500
+var graphw = 500
 
+//creación de lienzos
+
+var map_svg = d3.select('div')
+    .append('svg')
+    .attr('id','map')
+    .attr('width', mapw)
+    .attr('height', maph);
+
+var graph_svg = d3.select('div')
+    .append('svg')
+    .attr('id','graph')
+    .attr('width', graphw)
+    .attr('height', graphh);
 
 //Importación de datos
 d3.json('practica_airbnb.json').then((featureCollection) => {
     console.log(featureCollection);
+
+
+
+
+
+
+
+
 });
 
