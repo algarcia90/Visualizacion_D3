027187465bebde45b935265f12c393fa0f294a48
Alejandro Garcia <alejandro.nocedo@gmail.com>
2020-08-02 18:28:23 +0200

    Creo el mapa con los Path correspondientes

--- d3.js
+++ d3.js
@@ -29,6 +29,71 @@ d3.json('practica_airbnb.json').then((featureCollection) => {
 
 function mapMaker(featureCollection){
     console.log(featureCollection)
+
+    var projection = d3.geoMercator()
+        .fitSize([mapw, maph], featureCollection)
+
+    console.log(projection)
+
+    //preparo la función para crear paths
+    var pathProjection = d3.geoPath().projection(projection);
+    var features = featureCollection.features;
+
+    var createdPath = map_svg.selectAll('path')
+        .data(features)
+        .enter()
+        .append('path')
+        .attr('d', (d) => pathProjection(d))
+        .attr("opacity", function(d, i) {
+            d.opacity = 1
+            return d.opacity
+        });
+
+/* 
+    var scaleColor = d3.scaleOrdinal(d3.schemeTableau10);
+
+
+    createdPath.on('click', handleClick)
+        //Asignamos un color a cada path a traves de nuestra escala de colores
+    createdPath.attr('fill', (d) => scaleColor(d.properties.name));
+
+    //Creacion de una leyenda
+    var nblegend = 10;
+    var widthRect = (width / nblegend) - 2;
+    var heightRect = 10;
+
+    var scaleLegend = d3.scaleLinear()
+        .domain([0, nblegend])
+        .range([0, width]);
+        
+    var legend = svg.append("g")
+        .selectAll("rect")
+        .data(d3.schemeTableau10)
+        .enter()
+        .append("rect")
+        .attr("width", widthRect)
+        .attr("height", heightRect)
+        .attr("x", (d, i) => scaleLegend(i)) // o (i * (widthRect + 2)) //No haria falta scaleLegend
+        .attr("fill", (d) => d);
+
+    var text_legend = svg.append("g")
+        .selectAll("text")
+        .data(d3.schemeTableau10)
+        .enter()
+        .append("text")
+        .attr("x", (d, i) => scaleLegend(i)) // o (i * (widthRect + 2))
+        .attr("y", heightRect * 2.5)
+        .text((d) => d)
+        .attr("font-size", 12)
+
+    //Captura de eventos Click
+    function handleClick(d) {
+        d.opacity = d.opacity ? 0 : 1;
+        d3.select(this).attr('opacity', d.opacity);
+        console.log(d.properties.name);
+    }
+}*/
+
 };
 
 function pieMaker(featureCollection){
